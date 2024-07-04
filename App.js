b6e95efffdf8268fb32e2063a0f424da7f52342b
cold-flatwhite <canadasun2022@gmail.com>
2024-07-04 14:48:46 -0700

    added a TextInput and a state variable to control it

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 import Header from './Component/Header';
 
 export default function App() {
   const appName = "Summer 2004 Mobile";
+  const [text, setText] = useState('');
   return (
     <View style={styles.container}>
       <Header appName={appName}/>
+      <TextInput Style = {{height : 40}} 
+        placeholder='Type something'
+        onChangeText={newText => setText(newText) }
+        value={text}
+      />
+      <Text>{text}</Text>
       <StatusBar style="auto" />
     </View>
   );

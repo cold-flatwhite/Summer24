ea2a2a4619233b94625f6d7f213584c8a81a5367
cold-flatwhite <canadasun2022@gmail.com>
2024-07-09 15:05:19 -0700

    relative styling for input and app.js component

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { Button, SafeAreaView, StyleSheet, Text, TextInput, View } from "react-native";
 import Header from "./Component/Header";
 import Input from "./Component/Input";
 import { useState } from "react";
@@ -17,15 +17,23 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Header appName={appName} />
-      <Input inputHandler={handleInputData} isModalVisible = {modalVisible}/>
-      <Text>{receivedText}</Text>
+    <SafeAreaView style={styles.container}>
+      <View style = {styles.topContainer}>
+        <Header appName={appName} />
+        <Button
+          title="Add a goal"
+          onPress={() => {
+            setModalVisible(true);
+          }}
+        />
+      </View>
+      <View style = {styles.bottomContainer}>
+        <Input inputHandler={handleInputData} isModalVisible={modalVisible} />
+        <Text style={styles.textStyle}>{receivedText}</Text>
+      </View>
+
       <StatusBar style="auto" />
-      <Button title = "Add a goal" onPress={() => {
-        setModalVisible(true);
-      }}/>
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -33,7 +41,22 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
+    // alignItems: "center",
     justifyContent: "center",
   },
+  textStyle: {
+    color: "darkmagenta",
+  },
+  topContainer : {
+    flex : 1,
+    backgroundColor : "red",
+    alignItems : 'center',
+    justifyContent : 'center'
+  },
+  bottomContainer : {
+    flex : 4,
+    backgroundColor : "yellow",
+    alignItems : "center",
+    justifyContent : 'center'
+  }
 });
--- Component/Input.js
+++ Component/Input.js
@@ -1,9 +1,7 @@
 import { View, Text, TextInput, Button, StyleSheet, Modal } from "react-native";
 import React, { useState } from "react";
 
-
-const Input = ({inputHandler, isModalVisible}) => {
-
+const Input = ({ inputHandler, isModalVisible }) => {
   const [text, setText] = useState("");
   const [blur, setBlur] = useState(false);
   function handleConfirm() {
@@ -11,34 +9,35 @@ const Input = ({inputHandler, isModalVisible}) => {
     inputHandler(text);
   }
   return (
-    <Modal animationType="slide" visible = {isModalVisible}>
-    <View style={styles.container}>
-      <TextInput
-        autoFocus={true}
-        placeholder="Type password"
-        secureTextEntry={true}
-        onBlur={() => {
-          setBlur(true);
-        }}
-        onFocus={() => {
-          setBlur(false);
-        }}
-        onChangeText={(newText) => {
-          setText(newText);
-        }}
-        autoCapitalize={true}
-        value={text}
-      />
-      {blur && <Text>Thank you</Text>}
-      <Button
-        title="confirm"
-        onPress={() => {
-          handleConfirm();
-        }}
-      />
-    </View>
+    <Modal animationType="slide" visible={isModalVisible}>
+      <View style={styles.container}>
+        <TextInput
+          autoFocus={true}
+          placeholder="Type password"
+          secureTextEntry={true}
+          onBlur={() => {
+            setBlur(true);
+          }}
+          onFocus={() => {
+            setBlur(false);
+          }}
+          onChangeText={(newText) => {
+            setText(newText);
+          }}
+          autoCapitalize={true}
+          value={text}
+        />
+        {blur && <Text>Thank you</Text>}
+        <View style = {styles.buttonStyle}>
+          <Button
+            title="confirm"
+            onPress={() => {
+              handleConfirm();
+            }}
+          />
+        </View>
+      </View>
     </Modal>
-
   );
 };
 
@@ -49,7 +48,10 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  buttonStyle : {
+    marginHorizontal : '30%',
+    margin : 10,
+  }
 });
-
 
 export default Input;

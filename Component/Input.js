4c32b213492eb395f548bab6658d4fd49ff158bb
cold-flatwhite <canadasun2022@gmail.com>
2024-07-09 13:37:42 -0700

    adding Button and function handleConfirm to logs what user has typed in the TextInput to console.

--- Component/Input.js
+++ Component/Input.js
@@ -1,23 +1,36 @@
-import { View, Text, TextInput } from "react-native";
+import { View, Text, TextInput, Button } from "react-native";
 import React, { useState } from "react";
 
 const Input = () => {
   const [text, setText] = useState("");
-  const [focus, setFocus] = useState(true);
+  const [blur, setBlur] = useState(true);
+  function handleConfirm () {
+    console.log("user typed", text);
+  }
   return (
     <View>
       <TextInput
+        autoFocus={true}
         style={{ height: 40, borderColor: 'gray', borderWidth: 5}}
         placeholder="Type password"
         secureTextEntry={true}
+        onBlur={() => {
+          setBlur(true);
+        }}
+        onFocus={() => {
+          setBlur(false);
+        }}
         onChangeText={(newText) => {
           setText(newText);
           setFocus(true);
         }}
-        onBlur={() => setFocus(false)}
+
         value={text}
       />
-      {focus && <Text>Thank you</Text>}
+      {blur && <Text>Thank you</Text>}
+      <Button title="confirm" onPress = {() => {
+        handleConfirm();
+      }} />
     </View>
   );
 };

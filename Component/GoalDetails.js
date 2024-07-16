4fbac4fb833cab6827bd94c52de0d42e47c2ce8a
cold-flatwhite <canadasun2022@gmail.com>
2024-07-16 14:27:23 -0700

    added one buttion and use navigation.push to keep the previous page for each press

--- Component/GoalDetails.js
+++ Component/GoalDetails.js
@@ -1,14 +1,25 @@
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 import React from "react";
 
 export default function GoalDetails({ navigation, route }) {
   console.log(route.params);
   return (
     <View>
-      <Text>
-        You are seeing the details of goal with text :{route.params.goalObj.text} and id
-        {route.params.goalObj.id}:
-      </Text>
+      {route.params ? (
+        <Text>
+          You are seeing the details of goal with text :
+          {route.params.goalObj.text} and id
+          {route.params.goalObj.id}:
+        </Text>
+      ) : (
+        <Text>Details</Text>
+      )}
+      <Button
+        title="More Details"
+        onPress={() => {
+          navigation.push("Details");
+        }}
+      />
     </View>
   );
 }

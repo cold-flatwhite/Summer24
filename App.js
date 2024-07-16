952e26ba2a0efb447250e4504f7ad4ebae217b8d
cold-flatwhite <canadasun2022@gmail.com>
2024-07-16 14:39:16 -0700

    added button to header right

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 import React from "react";
 import Home from "./Component/Home";
 import { NavigationContainer } from "@react-navigation/native";
@@ -16,11 +16,27 @@ export default function App() {
           component={Home}
           options={{
             title: "Goal Lists",
-            headerStyle: { backgroundColor: 'darkmagenta' },
-            headerTintColor : 'white'
+            headerStyle: { backgroundColor: "darkmagenta" },
+            headerTintColor: "white",
           }}
         />
-        <Stack.Screen name="Details" component={GoalDetails} />
+        <Stack.Screen
+          name="Details"
+          component={GoalDetails}
+          options={({ navigation, route }) => {
+            return {
+              title: route.params.goalObj.text,
+              headerRight: () => {
+                return (
+                  <Button
+                    title="Warning"
+                    onPress={() => console.log("warning")}
+                  />
+                );
+              },
+            };
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );

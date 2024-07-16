8ea35a84dc6ee01f1b2c1cdd95d404a56f972629
cold-flatwhite <canadasun2022@gmail.com>
2024-07-16 13:37:30 -0700

    Made a native stack navigator and assgined Home as the first screen

--- App.js
+++ App.js
@@ -2,7 +2,16 @@ import { View, Text } from "react-native";
 import React from "react";
 import Home from "./Component/Home";
 import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return <NavigationContainer><Home/></NavigationContainer>;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }

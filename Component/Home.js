b8ae2d33a5750fb6661322bec6cde583c5834fdf
cold-flatwhite <canadasun2022@gmail.com>
2024-07-23 14:38:11 -0700

    writeToDb function was added

--- Component/Home.js
+++ Component/Home.js
@@ -10,24 +10,38 @@ import {
 } from "react-native";
 import Header from "./Header";
 import Input from "./Input";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import GoalItem from "./GoalItem";
 import PressableButton from "./PressableButton";
 import { app } from "../Firebase/firebaseSetup";
+import { writeToDB } from "../Firebase/firesotreHelper";
+import { database } from "../Firebase/firebaseSetup";
+import { collection, onSnapshot } from "firebase/firestore";
 
 export default function Home({navigation}) {
 
   const appName = "Summer 2024 Mobile";
   const [goals, setGoals] = useState([]);
   const [modalVisible, setModalVisible] = useState(false);
+  useEffect(() => {
+    onSnapshot(collection(database, "goal"), (querySnapShot) => {
+      let newArray = [];
+      if (!querySnapShot.empty) {
+        querySnapShot.forEach((docSnapShot) => {
+          console.log(docSnapShot.id)
+          newArray.push({...docSnapShot.data(), id : docSnapShot.id});
+        });
+      }
+      setGoals(newArray);
+    });
+  },[]);
 
   function handleInputData(data) {
-    const newGoal = { text: data, id: Math.random() };
+    const newGoal = { text: data };
     //use updater function when updating the state variable based on existing values
-    setGoals((currentGoals) => {
-      return [...currentGoals, newGoal];
-    });
-    console.log("callback fn called", data);
+    //call writeDB 
+    writeToDB(newGoal,"goal")
+
     setModalVisible(false);
   }
 
---
+++ Firebase/firesotreHelper.js
@@ -0,0 +1,10 @@
+import { addDoc, collection } from "firebase/firestore";
+import { database } from "./firebaseSetup";
+
+export async function writeToDB(data, collectionName) {
+  try {
+    await addDoc(collection(database, collectionName), data);
+  } catch (err) {
+    console.log(err);
+  }
+}
